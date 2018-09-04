8f2851d9e79a26c3324defd459ef3d442dca8cba
Jamie Brown <jmbrowncow@gmail.com>
2018-09-04 14:39:20 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     @auth = auth
     @user = User.find_or_create_by_omniauth(@auth)
+    raise @user.inspect
     session[:user_id] = @user.id
   end
 
