66b5957483fff63bf128f94a5781ba60c237bddd
Jamie Brown <jmbrowncow@gmail.com>
2018-09-04 14:30:21 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(:email => auth_hash['info']['email']).first_or_create do |user|
+      user.name = auth_hash['info']['name']
+      user.image = auth_hash['info']['image']
+      user.uid = auth_hash['info']['uid']
+    end
+  end
 end
