885ab8a84c091b0b678c406af7fc39973f3804a5
Jamie Brown <jmbrowncow@gmail.com>
2018-09-04 14:48:53 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
     self.where(:uid => auth_hash['uid']).first_or_create do |user|
       user.name = auth_hash['info']['name']
       user.image = auth_hash['info']['image']
-      user.email = auth_hash['info']['email']    
+      user.email = auth_hash['info']['email']
     end
   end
 end
